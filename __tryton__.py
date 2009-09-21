9cdc7ad27a75ded2d7f7dd85ee67d5297e9f522f
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-09-21 14:33:30 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'LDAP Connection',
+    'name_de_DE' : 'LDAP Verbindung',
     'version': '1.3.0',
     'author': 'B2CK, Josh Dukes & Udo Spallek',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Add basic support for LDAP connection.''',
+    'description_de_DE' : '''LDAP Verbindung
+    - Fügt Basisunterstützung für Verbindungen zu einem LDAP-Server hinzu
+''',
     'depends': [
         'ir',
         'res',
@@ -15,5 +19,6 @@
         'connection.xml',
     ],
     'translation': [
+        'de_DE.csv',
     ],
 }
