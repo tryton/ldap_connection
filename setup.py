1e5cb1fd1473940fba52b88a1be6311807a240cd
Cédric Krier <ced@b2ck.com>
2010-03-12 17:49:37 +0100

    Add test_suite in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='trytond_ldap_connection',
     package_dir={'trytond.modules.ldap_connection': '.'},
     packages=[
         'trytond.modules.ldap_connection',
+        'trytond.modules.ldap_connection.tests',
     ],
     package_data={
         'trytond.modules.ldap_connection': info.get('xml', []) \
@@ -58,4 +59,6 @@ setup(name='trytond_ldap_connection',
     [trytond.modules]
     ldap_connection = trytond.modules.ldap_connection
     """,
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
 )
